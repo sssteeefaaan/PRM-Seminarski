b46cfa0ae63378b9cfabd18065079a76ce233f10
Marko Arandjelovic <marko977x@gmail.com>
2021-11-23 15:48:42 +0100

    update

--- code/main2.py
+++ code/main2.py
@@ -2,16 +2,19 @@ from visual import visualizeDataFileArray
 from compareFuncs import *
 import json
 
+
 def main():
     with open('config.json') as f:
         config = json.load(f)
-    
+
     attr, display, data_source, optional = config.values()
-        
-    cmpMeta = [(config['attributes'][x]['xml_parser'], config['attributes'][x]['c'], funcs[config['attributes'][x]['h']]) for x in config['attributes'] if config['attributes'][x]['use']]
-    
+
+    cmpMeta = [(attr[x]['xml_parser'], attr[x]['c'], funcs[attr[x]['h']])
+               for x in attr if attr[x]['use']]
+
     (a, c, h) = zip(*cmpMeta)
-    visualizeDataFileArray(data_source['urls'], a, c, h, display['t'])
-        
+    visualizeDataFileArray(data_source['urls'], a, c, h, display=display)
+
+
 if __name__ == '__main__':
-        main()
+    main()
--- code/visual.py
+++ code/visual.py
@@ -9,129 +9,123 @@ from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, thread
 from threading import Thread
 from multiprocessing import Process
 
-colors = ['#FFFF00', '#66CDAA', '#6495ED', '#DD0000', '#FF1493', '#FEE4C4', '#5F9EA0', '#808080', 
-          '#FFD700', '#32CD32', '#ADD8E6', '#FF7F50', '#BA55D3', '#DEB887', '#DB7093', '#A0ACA0', 
-          '#DAA520', '#9ACD32', '#4169E1', '#CD5C5C', '#DD00DD', '#A53A2A', '#778899', '#CFD5C5', 
-          '#EEE8AA', '#40E0D0', '#1E90FF', '#DC143C', '#D8BFD8', '#B8860B', '#F0F890', '#505050']
 
-def visualizeDataFileArray(urls, attributes, c, h, t, time = 15, size=5):
+# , t=0.5, refresh_rate=0.01, fifo_count=100, node_colors=[], layout_ind=0):
+def visualizeDataFileArray(urls, attributes, c, h, display={}):
     files = downloadXMLFiles(urls)
-    
+
+#
+    t = display['t'] if display['t'] else 0.5
+    refresh_rate = display['refresh_rate'] if display['refresh_rate'] else 0.0001
+    fifo_count = display['fifo_count'] if display['fifo_count'] else 100
+    node_colors = display['node_colors'] if display['node_colors'] else []
+    layout_ind = display['layout_ind'] if display['layout_ind'] else 0
+#
     G = nx.Graph()
     A = []
     color_map = []
-    
+
     mng = plt.get_current_fig_manager()
     mng.set_window_title('Vizualizacija klasterizacije IDS alerta')
     fig = plt.figure(1)
-    
+
     def on_key(event):
         if event.key == ' ':
             fig.canvas.start_event_loop(0)
         if event.key == 'escape':
             fig.canvas.stop_event_loop()
-            
-    # def compareEdge(rank, num):
-    #     for j in range(0, num):
-    #         ind = j * size + rank
-    #         if len(A) > ind:
-    #             weight = F(a.vector, A[ind].vector, c, h)
-    #             if weight >= t:
-    #                 G.add_weighted_edges_from([(a.dict['alertid'], A[ind].dict['alertid'], weight)])
-        
+
     fig.canvas.mpl_connect('key_release_event', on_key)
-    
+
     i = 1
     mypos = None
     for file in files:
         name = file.replace(".xml", "").replace("downloads/", "")
         parsedData = parseXML(file)
-        #interval = time / ((len(parsedData)) << 2)
         partial = []
 
         for row in parsedData:
             row['alertid'] = len(A) + 1
             a = Alert(attributes, row)
-        
+
             G.add_nodes_from([
                 (a.dict['alertid'], {
                     # attributes
-                    #'key': 'value'
-                    })
-                ])
-            color_map.append(colors[i - 1])
-            
+                    # 'key': 'value'
+                })
+            ])
+            color_map.append(node_colors[i - 1])
+
             for node in A:
                 weight = F(a.vector, node.vector, c, h)
                 if weight >= t:
-                    G.add_weighted_edges_from([(a.dict['alertid'], node.dict['alertid'], {'t':weight})])
-                        # attributes
-                        #'key': 'value'
-                        # weight = str(weight),
-                        # label = str(weight)
-                        
-                        
-            # threads = []
-            # num = len(A) // size
-            # for tRank in range(0, size):
-            #     threads.append(Thread(target=compareEdge, args=[tRank, num]))
-            #     threads[-1].start()
-                
-            # for thread in threads:
-            #     thread.join()
-                        
+                    G.add_weighted_edges_from(
+                        [(a.dict['alertid'], node.dict['alertid'], weight)])
+                    # attributes
+                    # 'key': 'value'
+                    # weight = str(weight),
+                    # label = str(weight)
+
             partial.append(a)
             A.append(a)
-            
-            mypos = nx.kamada_kawai_layout(G, weight='t')
-            #mypos = nx.spring_layout(G, k=1, seed=100, pos=mypos)#, scale=0.75, center=(0,0), fixed=None)
+
+            if layout_ind == 0:
+                mypos = nx.kamada_kawai_layout(G)
+            elif layout_ind == 1:
+                # , scale=0.75, center=(0,0), fixed=None)
+                mypos = nx.spring_layout(G, k=1, seed=100, pos=mypos)
+
             nx.draw_networkx(G, pos=mypos, node_color=color_map)
             plt.title(name)
-            plt.pause(0.00001)
+            plt.pause(refresh_rate)
             plt.clf()
-        
-        savetoCSV(list(map(lambda x: x.dict, partial)), str(i) + '. ' + name)
-        i = (i + 1) % len(colors)
 
-    th1  = Thread(target = savetoCSV, args = [list(map(lambda x: x.dict, A)), 'Data'])
+        savetoCSV(list(map(lambda x: x.dict, partial)), str(i) + '. ' + name)
+        i = (i + 1) % len(node_colors)
+
+    th1 = Thread(target=savetoCSV, args=[
+                 list(map(lambda x: x.dict, A)), 'Data'])
     th1.start()
-    
+
     clusters = list(nx.find_cliques(G))
-    #color_map = [-1] * len(color_map)
-    
+
     i = 0
     for cluster in clusters:
         for node in cluster:
-            #if color_map[node - 1] == -1:
-            color_map[node - 1] = colors[i]
-        i = (i + 1) % len(colors)
-    
-    mypos = nx.kamada_kawai_layout(G)
-    #mypos = nx.spring_layout(G, k=1, seed=100, pos=mypos)#, center=(0,0))
+            # if color_map[node - 1] == -1:
+            color_map[node - 1] = node_colors[i]
+        i = (i + 1) % len(node_colors)
+
+    if layout_ind == 0:
+        mypos = nx.kamada_kawai_layout(G)
+    elif layout_ind == 1:
+        # , scale=0.75, center=(0,0), fixed=None)
+        mypos = nx.spring_layout(G, k=1, seed=100, pos=mypos)
+
     nx.draw_networkx(G, pos=mypos, node_color=color_map)
     plt.show()
-    
+
     th1.join()
 
-### ne koristi
+# ne koristi
 # def visualizeDataURL(url, attributes, c, h, t, time = 15):
 #     parsedData = parseXMLFromUrl(url)
 
 #     G = nx.Graph()
 #     A = []
-    
+
 #     interval = time / ((len(parsedData)) << 2)
 
 #     for row in parsedData:
 #         a = Alert(attributes, row)
-    
+
 #         G.add_nodes_from([
 #             (a.dict['alertid'], {
 #                 # attributes
 #                 #'key': 'value'
 #                 })
 #             ])
-        
+
 #         for node in A:
 #             weight = F(a.vector, node.vector, c, h)
 #             if weight >= t:
@@ -140,24 +134,24 @@ def visualizeDataFileArray(urls, attributes, c, h, t, time = 15, size=5):
 #                     #'key': 'value'
 #                     # weight = str(weight),
 #                     # label = str(weight)
-                    
+
 #         mypos = nx.spring_layout(G, seed=100)
 #         nx.draw_networkx(G, pos=mypos)
 #         plt.pause(interval)
 #         plt.clf()
-        
+
 #         A.append(a)
 
 #     savetoCSV(list(map(lambda x: x.dict, A)), getFileNameFromUrl(url) + '.csv')
 #     nx.draw_networkx(G)
 #     plt.show()
-    
+
 # def visualizeDataURLArray(urls, attributes, c, h, t, time = 10):
 #     G = nx.Graph()
 #     A = []
 #     i = 1
 #     color_map = []
-    
+
 #     for url in urls:
 #         print(f'Downloading: \'{url}\'')
 #         name = '-'.join(url.split('/')[-4:])
@@ -168,7 +162,7 @@ def visualizeDataFileArray(urls, attributes, c, h, t, time = 15, size=5):
 #         for row in parsedData:
 #             row['alertid'] = len(A) + 1
 #             a = Alert(attributes, row)
-        
+
 #             G.add_nodes_from([
 #                 (a.dict['alertid'], {
 #                     # attributes
@@ -176,7 +170,7 @@ def visualizeDataFileArray(urls, attributes, c, h, t, time = 15, size=5):
 #                     })
 #                 ])
 #             color_map.append(colors[i - 1])
-            
+
 #             for node in A:
 #                 weight = F(a.vector, node.vector, c, h)
 #                 if weight >= t:
@@ -185,16 +179,16 @@ def visualizeDataFileArray(urls, attributes, c, h, t, time = 15, size=5):
 #                         #'key': 'value'
 #                         # weight = str(weight),
 #                         # label = str(weight)
-                        
+
 #             mypos = nx.spring_layout(G, seed=100)
 #             nx.draw_networkx(G, pos=mypos, node_color=color_map)
 #             plt.title(name)
 #             plt.pause(0.00001)
 #             plt.clf()
-            
+
 #             partial.append(a)
 #             A.append(a)
-        
+
 #         savetoCSV(list(map(lambda x: x.dict, partial)), 'data/' + str(i) + '. ' + name + '.csv')
 #         i = (i + 1)%len(colors)
 
@@ -209,17 +203,17 @@ def visualizeDataFileArray(urls, attributes, c, h, t, time = 15, size=5):
 #     savetoCSV(list(map(lambda x: x.dict, A)), 'data/Data.csv')
 #     nx.draw_networkx(G, node_color=color_map)
 #     plt.show()
-    
+
 # def parseURL(url):
 #     print(f'Downloading \'{url}\'')
 #     return ('-'.join(url.split('/')[-4:]), parseXMLFromUrl(url))
-                            
+
 # def visualizeDataURLArrayMP(urls, attributes, c, h, t, time = 10, size=5):
     G = nx.Graph()
     A = []
     i = 1
     color_map = []
-    
+
     def checkEdge(rank):
         length = len(A)
         for i in range(0, num):
@@ -227,15 +221,16 @@ def visualizeDataFileArray(urls, attributes, c, h, t, time = 15, size=5):
             if length > index:
                 weight = F(a.vector, A[index].vector, c, h)
                 if weight >= t:
-                    G.add_weighted_edges_from([(a.dict['alertid'], A[index].dict['alertid'], weight)])
+                    G.add_weighted_edges_from(
+                        [(a.dict['alertid'], A[index].dict['alertid'], weight)])
                     # attributes
-                    #'key': 'value'
+                    # 'key': 'value'
                     # weight = str(weight),
                     # label = str(weight)
-    
+
     with ThreadPoolExecutor() as executor:
         results = executor.map(parseURL, urls)
-        
+
     for res in results:
         name, parsedData = res
         partial = []
@@ -243,35 +238,36 @@ def visualizeDataFileArray(urls, attributes, c, h, t, time = 15, size=5):
         for row in parsedData:
             row['alertid'] = len(A) + 1
             a = Alert(attributes, row)
-        
+
             G.add_nodes_from([
                 (a.dict['alertid'], {
                     # attributes
-                    #'key': 'value'
-                    })
-                ])
+                    # 'key': 'value'
+                })
+            ])
             color_map.append(colors[i-1])
-            
+
             num = len(A) // size
             threads = []
             for rank in range(0, size):
                 threads.append(Thread(target=checkEdge, args=[rank]))
                 threads[rank].start()
-                
+
             for thread in threads:
                 thread.join()
-            
+
             mypos = nx.spring_layout(G, seed=100)
             nx.draw_networkx(G, pos=mypos, node_color=color_map)
             plt.title(name)
             plt.pause(0.00001)
             plt.clf()
-                
+
             partial.append(a)
             A.append(a)
-        
-        savetoCSV(list(map(lambda x: x.dict, partial)), 'data/' + str(i) + '. ' + name + '.csv')
-        i+=1
+
+        savetoCSV(list(map(lambda x: x.dict, partial)),
+                  'data/' + str(i) + '. ' + name + '.csv')
+        i += 1
 
     clusters = list(nx.find_cliques(G))
     i = 0
@@ -280,7 +276,7 @@ def visualizeDataFileArray(urls, attributes, c, h, t, time = 15, size=5):
         for node in cluster:
             if color_map[node - 1] == -1:
                 color_map[node-1] = colors[i]
-        i = (i + 1)%len(colors)
+        i = (i + 1) % len(colors)
     savetoCSV(list(map(lambda x: x.dict, A)), 'data/Data.csv')
     nx.draw_networkx(G, node_color=color_map)
     plt.show()
